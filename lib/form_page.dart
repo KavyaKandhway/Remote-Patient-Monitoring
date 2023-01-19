1033a56e27ba9aa6c155a4d2e724b3ef5b53d127
Kavya Kandhway <kandhwaykavya@gmail.com>
2023-01-19 18:09:17 +0530

    aa

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -1,7 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:remote_patient_monitoring/clipShadowPath.dart';
 import 'package:remote_patient_monitoring/home_page.dart';
+import 'package:remote_patient_monitoring/values.dart';
+import 'clipper.dart';
+
+const List<Widget> genders = <Widget>[
+  Text('Male'),
+  Text('Female'),
+];
 
 class FormPage extends StatefulWidget {
   const FormPage({Key? key}) : super(key: key);
@@ -13,190 +21,189 @@ class FormPage extends StatefulWidget {
 class _FormPageState extends State<FormPage> {
   final nameController = TextEditingController();
   final ageController = TextEditingController();
+  final contactController = TextEditingController();
   final allergyController = TextEditingController();
   String? gender;
+  final List<bool> _selectedgender = <bool>[true, false];
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     final db = FirebaseFirestore.instance;
     final curUser = FirebaseAuth.instance.currentUser;
+    var heightOfScreen = MediaQuery.of(context).size.height;
+    var widthOfScreen = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Welcome!",
-              style: TextStyle(fontSize: 30),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Please fill the details of the patient",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Enter the name",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: nameController,
-                cursorColor: Colors.black87,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                      borderRadius: BorderRadius.circular(10.0),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipShadowPath(
+                clipper: CustomSignUpShapeClipper2(),
+                shadow: const Shadow(blurRadius: 24, color: Color(0xFF4045EE)),
+                child: Container(
+                  height: heightOfScreen * 0.3,
+                  width: widthOfScreen,
+                  color: const Color(0xFF4045EE),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: heightOfScreen * 0.05,
+                        ),
+                        Text(
+                          "Patient Details",
+                          style: theme.textTheme.headline4?.copyWith(
+                            color: const Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ],
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    labelText: 'Name of the patient'),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Enter the age",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: ageController,
-                cursorColor: Colors.black87,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    labelText: 'Age of the patient'),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Select Gender",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                RadioListTile(
-                  title: Text("Male"),
-                  value: "male",
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value.toString();
-                    });
-                  },
-                ),
-                RadioListTile(
-                  title: Text("Female"),
-                  value: "female",
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value.toString();
-                    });
-                  },
-                ),
-              ],
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Allergies (if any)",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: allergyController,
-                cursorColor: Colors.black87,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                        width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                        width: 3, color: Colors.greenAccent), //<-- SEE HERE
-                    borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                  minimumSize: Size.fromHeight(50),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: widthOfScreen - 30,
+                child: CustomTextFormField(
+                  textEditingController: nameController,
+                  textInputType: TextInputType.name,
+                  labelText: "Name",
+                  border: Borders.customOutlineInputBorder(),
+                  enabledBorder: Borders.customOutlineInputBorder(),
+                  focusedBorder: Borders.customOutlineInputBorder(
+                    color: AppColors.violetShade200,
+                  ),
+                  labelStyle: Styles.customTextStyle(),
+                  hintTextStyle: Styles.customTextStyle(),
+                  textStyle: Styles.customTextStyle(),
                 ),
-                icon: const Icon(
-                  Icons.send_rounded,
-                  size: 32,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: widthOfScreen - 30,
+                child: CustomTextFormField(
+                  textEditingController: ageController,
+                  textInputType: TextInputType.number,
+                  labelText: "Age",
+                  border: Borders.customOutlineInputBorder(),
+                  enabledBorder: Borders.customOutlineInputBorder(),
+                  focusedBorder: Borders.customOutlineInputBorder(
+                    color: AppColors.violetShade200,
+                  ),
+                  labelStyle: Styles.customTextStyle(),
+                  hintTextStyle: Styles.customTextStyle(),
+                  textStyle: Styles.customTextStyle(),
                 ),
-                label: const Text(
-                  'Submit',
-                  style: TextStyle(fontSize: 24),
-                ),
-                onPressed: () {
-                  final user = <String, dynamic>{
-                    "name": nameController.text.trim(),
-                    "age": ageController.text.trim(),
-                    "gender": gender,
-                    "allergies": allergyController.text.trim(),
-                  };
-                  db.collection("users").doc(curUser?.uid).set(user);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                  );
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ToggleButtons(
+                direction: Axis.horizontal,
+                onPressed: (int index) {
+                  setState(() {
+                    // The button that is tapped is set to true, and the others to false.
+                    for (int i = 0; i < _selectedgender.length; i++) {
+                      _selectedgender[i] = i == index;
+                    }
+                  });
                 },
+                borderRadius: const BorderRadius.all(Radius.circular(50)),
+                selectedBorderColor: Color(0xFF4045EE),
+                selectedColor: Colors.white,
+                fillColor: Color(0xFF4045EE),
+                color: Color(0xFF4045EE),
+                constraints: const BoxConstraints(
+                  minHeight: 50.0,
+                  maxWidth: 250,
+                  minWidth: 180,
+                ),
+                isSelected: _selectedgender,
+                children: genders,
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: widthOfScreen - 30,
+                child: CustomTextFormField(
+                  textEditingController: contactController,
+                  textInputType: TextInputType.number,
+                  labelText: "Emergency Contact",
+                  border: Borders.customOutlineInputBorder(),
+                  enabledBorder: Borders.customOutlineInputBorder(),
+                  focusedBorder: Borders.customOutlineInputBorder(
+                    color: AppColors.violetShade200,
+                  ),
+                  labelStyle: Styles.customTextStyle(),
+                  hintTextStyle: Styles.customTextStyle(),
+                  textStyle: Styles.customTextStyle(),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: widthOfScreen - 30,
+                child: CustomTextFormField(
+                  textEditingController: allergyController,
+                  textInputType: TextInputType.text,
+                  labelText: "Allergies(if any)",
+                  border: Borders.customOutlineInputBorder(),
+                  enabledBorder: Borders.customOutlineInputBorder(),
+                  focusedBorder: Borders.customOutlineInputBorder(
+                    color: AppColors.violetShade200,
+                  ),
+                  labelStyle: Styles.customTextStyle(),
+                  hintTextStyle: Styles.customTextStyle(),
+                  textStyle: Styles.customTextStyle(),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 250,
+                decoration: Decorations.customBoxDecoration(blurRadius: 10),
+                child: CustomButton(
+                  title: "Submit",
+                  elevation: 12,
+                  textStyle: theme.textTheme.titleMedium?.copyWith(
+                    color: AppColors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  color: AppColors.blue,
+                  height: 50,
+                  onPressed: () {
+                    if (_selectedgender[0]) {
+                      gender = 'male';
+                    } else {
+                      gender = 'female';
+                    }
+                    final user = <String, dynamic>{
+                      "name": nameController.text.trim(),
+                      "age": ageController.text.trim(),
+                      "gender": gender,
+                      "allergies": allergyController.text.trim(),
+                    };
+                    db.collection("users").doc(curUser?.uid).set(user);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
